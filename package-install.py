006d7da374cfbea23fdb0ae7f9e7ee6b13501beb
MRPodle <vreg2391@outlook.com>
2023-09-20 14:37:40 -0400

    testing requirement installer

---
+++ package-install.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def install_dependencies():
+    try:
+        subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
+        print("Dependencies installed successfully")
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing dependencies: {e}")
+
+if __name__ == "__main__":
+    install_dependencies()
