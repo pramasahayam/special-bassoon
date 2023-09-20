e4d0fb763e8c23243970235ac88412c643064546
MRPodle <vreg2391@outlook.com>
2023-09-20 14:50:07 -0400

    successfully created package installer file and requirements.txt

--- package-install.py
+++ package-install.py
@@ -1,11 +1,13 @@
 import subprocess
 
+
 def install_dependencies():
     try:
-        subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
-        print("Dependencies installed successfully")
+        subprocess.check_call(["pip", "install", "-r", "requirements.txt", "--user"])
+        print("Dependencies installed successfully.")
+        
     except subprocess.CalledProcessError as e:
-        print(f"Error installing dependencies: {e}")
+        print(f"Error installing dependencies: {e}")   
 
 if __name__ == "__main__":
     install_dependencies()
