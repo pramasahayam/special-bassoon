4b9b3569261eb3fa3e4a43d9a2be7bee06a9c203
MRPodle <vreg2391@outlook.com>
2023-10-03 23:00:06 -0400

    added funny orbiting

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import tkinter as tk
+import pygame
 from math import cos, sin, radians
-
 from space_bodies import Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto, Lua
 
-class SpaceApp:
-    def __init__(self, root):
-        self.canvas = tk.Canvas(root, bg="black", width=1600, height=1200)
-        self.canvas.pack(padx=10, pady=10)
+# Initialize pygame
+pygame.init()
 
-        # Initializes Sun and all planets
+# Constants
+WIDTH, HEIGHT = 1600, 1200
+BACKGROUND_COLOR = (0, 0, 0)
+
+# Create screen and clock for controlling frame rate
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Solar System Simulation")
+clock = pygame.time.Clock()
+
+class SpaceApp:
+    def __init__(self):
+        # Initialize celestial bodies
         self.sun = Sun(800, 400)
         self.mercury = Mercury(860, 400)
         self.venus = Venus(920, 400)
@@ -20,80 +28,49 @@ class SpaceApp:
         self.neptune = Neptune(1280, 400)
         self.pluto = Pluto(1340, 400)
         self.lua = Lua(self.earth.x + 30, self.earth.y)
+        
+        self.bodies = [self.sun, self.mercury, self.venus, self.earth, self.mars, self.jupiter, self.saturn, self.uranus, self.neptune, self.pluto, self.lua]
+        self.angle = 0
 
-        # Creates visual representations on the canvas
-        self.sun_id = self.create_space_body(self.sun)
-        self.mercury_id = self.create_space_body(self.mercury)
-        self.venus_id = self.create_space_body(self.venus)
-        self.earth_id = self.create_space_body(self.earth)
-        self.mars_id = self.create_space_body(self.mars)
-        self.jupiter_id = self.create_space_body(self.jupiter)
-        self.saturn_id = self.create_space_body(self.saturn)
-        self.uranus_id = self.create_space_body(self.uranus)
-        self.neptune_id = self.create_space_body(self.neptune)
-        self.pluto_id = self.create_space_body(self.pluto)
-        self.lua_id = self.create_space_body(self.lua)
+    def draw_space_body(self, body):
+        pygame.draw.circle(screen, body.color, (int(body.x), int(body.y)), body.radius)
 
-        # Rotates bodies
-        self.rotate_space_body(self.mercury, self.mercury_id)
-        self.rotate_space_body(self.venus, self.venus_id)
-        self.rotate_space_body(self.earth, self.earth_id)
-        self.rotate_space_body(self.mars, self.mars_id)
-        self.rotate_space_body(self.jupiter, self.jupiter_id)
-        self.rotate_space_body(self.saturn, self.saturn_id)
-        self.rotate_space_body(self.uranus, self.uranus_id)
-        self.rotate_space_body(self.neptune, self.neptune_id)
-        self.rotate_space_body(self.pluto, self.pluto_id)
-        self.rotate_space_body_around_center(self.lua, self.lua_id, self.earth)
+    def run(self):
+        running = True
+        while running:
+            screen.fill(BACKGROUND_COLOR)
+            for body in self.bodies:
+                self.draw_space_body(body)
+            
+            # Rotate planets
+            self.rotate_space_body(self.mercury, self.sun)
+            self.rotate_space_body(self.venus, self.sun)
+            self.rotate_space_body(self.earth, self.sun)
+            self.rotate_space_body(self.mars, self.sun)
+            self.rotate_space_body(self.jupiter, self.sun)
+            self.rotate_space_body(self.saturn, self.sun)
+            self.rotate_space_body(self.uranus, self.sun)
+            self.rotate_space_body(self.neptune, self.sun)
+            self.rotate_space_body(self.pluto, self.sun)
+            
+            # Rotate Lua around Earth
+            self.rotate_space_body(self.lua, self.earth)
+            
+            self.angle += 0.5
+            
+            pygame.display.flip()
+            clock.tick(60)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-    def create_space_body(self, body):
-        return self.canvas.create_oval(
-            body.x - body.radius, body.y - body.radius,
-            body.x + body.radius, body.y + body.radius,
-            fill=body.color
-        )
+    def rotate_space_body(self, body, center_body):
+        orbit_radius = ((center_body.x - body.x) ** 2 + (center_body.y - body.y) ** 2) ** 0.5
+        body.x = center_body.x + orbit_radius * cos(radians(self.angle))
+        body.y = center_body.y + orbit_radius * sin(radians(self.angle))
 
-    def rotate_space_body(self, body, body_id):
-        angle = 0
-        orbit_radius = ((self.sun.x - body.x)**2 + (self.sun.y - body.y)**2)**0.5
-
-        def update_position():
-            nonlocal angle
-            body.x = self.sun.x + orbit_radius * cos(radians(angle))
-            body.y = self.sun.y + orbit_radius * sin(radians(angle))
-            self.canvas.coords(
-                body_id,
-                body.x - body.radius, body.y - body.radius,
-                body.x + body.radius, body.y + body.radius
-            )
-
-            angle_increment = 360 / (body.rotation_period * 365.25)
-            angle += angle_increment
-            root.after(50, update_position)
-
-        update_position()
-
-    def rotate_space_body_around_center(self, body, body_id, center_body):
-        angle = 0
-        orbit_radius = ((center_body.x - body.x)**2 + (center_body.y - body.y)**2)**0.5
-
-        def update_position():
-            nonlocal angle
-            body.x = center_body.x + orbit_radius * cos(radians(angle))
-            body.y = center_body.y + orbit_radius * sin(radians(angle))
-            self.canvas.coords(
-                body_id,
-                body.x - body.radius, body.y - body.radius,
-                body.x + body.radius, body.y + body.radius
-            )
-
-            angle_increment = 360 / (body.rotation_period * 27.3)
-            angle += angle_increment
-            root.after(50, update_position)
-
-        update_position()
-
-root = tk.Tk()
-root.title("Solar System Simulation")
-app = SpaceApp(root)
-root.mainloop()
+if __name__ == "__main__":
+    app = SpaceApp()
+    app.run()
+    pygame.quit()
