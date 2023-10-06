2828f653ced75e04e234eba74b6dc509f131e289
MRPodle <vreg2391@outlook.com>
2023-10-05 22:45:38 -0400

    added tempcoderunner files to .gitignore and fixed aspect ratio stretching when maximizing window on a vertical monitor

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
 from space_bodies import Sun, Earth, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto 
 
-# Constants for screen dimensions
-WIDTH, HEIGHT = 800, 600
-
-# Zooming and panning
+# Zooming and panning parameters
 LINEAR_ZOOM_AMOUNT = 100.0
 dragging = False
 last_mouse_x, last_mouse_y = 0, 0
@@ -17,13 +13,21 @@ CAMERA_DISTANCE = INITIAL_CAMERA_DISTANCE
 MIN_ZOOM_IN = -200
 MAX_ZOOM_OUT = -5000
 
+def resize(WIDTH, HEIGHT):
+    """Handles the window resize."""
+    glViewport(0, 0, WIDTH, HEIGHT)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, (WIDTH / HEIGHT), 0.1, 5000.0)
+    glMatrixMode(GL_MODELVIEW)
+
 def draw_body(body, t):
     glColor3fv(body.color)
     quad = gluNewQuadric()
     slices, stacks = 100, 100
     glPushMatrix()
     x, y, z = body.compute_position(t)
-    glTranslatef(x * 500, y * 500, z * 500)  # Adjusted scaling factor for visualization
+    glTranslatef(x * 1000, y * 1000, z * 1000)  # Scaling factor for visualization
     gluSphere(quad, body.radius, slices, stacks)
     glPopMatrix()
 
@@ -31,11 +35,11 @@ def main():
     global dragging, last_mouse_x, last_mouse_y, CAMERA_DISTANCE
 
     pygame.init()
-    display = (WIDTH, HEIGHT)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    glEnable(GL_DEPTH_TEST)  # Enable depth testing
-
-    gluPerspective(45, (display[0] / display[1]), 0.1, 5000.0)
+    # Set screen dimensions to the system's dimensions
+    WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL | pygame.RESIZABLE)
+    pygame.display.set_caption('Solar System Simulation')
+    resize(WIDTH, HEIGHT)  # Adjust the viewport and aspect ratio
     glTranslatef(0, 0, CAMERA_DISTANCE)
 
     sun = Sun()
@@ -49,8 +53,8 @@ def main():
     neptune = Neptune()
     pluto = Pluto()
     space_bodies = [sun, earth, mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto]
-    
-    t = sun.ts.now()  # Initial time
+
+    t = sun.ts.now()  # Current time
 
     while True:
         for event in pygame.event.get():
@@ -58,6 +62,10 @@ def main():
                 case pygame.QUIT:
                     pygame.quit()
                     return
+
+                case pygame.VIDEORESIZE:
+                    WIDTH, HEIGHT = event.size
+                    resize(WIDTH, HEIGHT)
 
                 case pygame.MOUSEBUTTONDOWN:
                     match event.button:
@@ -87,15 +95,11 @@ def main():
                         glTranslatef(dx * 0.5, -dy * 0.5, 0)
                         last_mouse_x, last_mouse_y = mouse_x, mouse_y
 
-        t = sun.ts.tt(jd=t.tt + 0.1)  # Adjusting the time progression
-
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         for body in space_bodies:
             draw_body(body, t)
         pygame.display.flip()
         pygame.time.wait(10)
-
-
 
 if __name__ == "__main__":
     main()
