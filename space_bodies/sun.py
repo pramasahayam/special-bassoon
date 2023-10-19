b7cf70595db928e423d385ef609b3073db965709
Angelina Buysse <angelina.buysse@gmail.com>
2023-10-19 11:58:18 -0400

    add day and year

--- space_bodies/sun.py
+++ space_bodies/sun.py
@@ -8,7 +8,6 @@ class Sun(SpaceBody):
             skyfield_name='sun', 
             data_url='de421.bsp',  # The specific URL or file path for the sun's data
             name="Sun",
-            age="4.57 billion years",
             description="The sun is the center of the solar system and is a dynamic yellow dwarf star, constantly changing and sending energy out into space. It is a hot ball of plasma, inflated and heated by nuclear fusion reactions at its core. Internal energy is emitted as light, ultraviolet, and infrared radiation.",
             mass="1.99E+30 kg",
             diameter="1,400,000 km",
