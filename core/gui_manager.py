0d02d0bad873cd8878a22a2f9c8ec6e397d742d5
MRPodle <vreg2391@outlook.com>
2023-11-07 23:56:20 -0500

    broke up selector menu function into 5 respective functions

--- core/gui_manager.py
+++ core/gui_manager.py
@@ -78,55 +78,55 @@ class GuiManager:
         return self.is_using_imgui
 
     def render_celestial_body_selector(self, solar_system, user_interactions, date_manager):
-        """
-        Renders a dropdown menu to select a celestial body, with bodies categorized by their 'category' attribute.
-        :param solar_system: An instance of the SolarSystem class
-        """
+        # Initialize Window
         imgui.set_next_window_position(366, 0)
         self.set_common_style()
         imgui.begin("Celestial Body Selector", flags=imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_SCROLLBAR | imgui.WINDOW_NO_MOVE | imgui.WINDOW_ALWAYS_AUTO_RESIZE)
         imgui.push_style_color(imgui.COLOR_BUTTON, 0.0, 0.5, 0.8, 1.0)
-        # Sort celestial bodies into categories
-        categories = {}
-        for body in solar_system.space_bodies:
-            category = body.category  # Access the category attribute of the celestial body
-            if category not in categories:
-                categories[category] = []
-            categories[category].append(body.name)
 
+        # Prepare ComboBox
         current_selection_label = solar_system.selected_planet.name if solar_system.selected_planet else "Select Object"
-
         desired_width = 125
         imgui.push_item_width(desired_width)
 
-        # Start the combo box
+        # Populate Categories and Handle Selection
         if imgui.begin_combo("##celestial_body_combo", current_selection_label):
-            for category, bodies in categories.items():
-                # Create a tree node for each category
-                if imgui.tree_node(category):
-                    for body_name in bodies:
-                        # When a selectable item is clicked, update the selected planet in the solar_system
-                        _, selected = imgui.selectable(body_name, solar_system.selected_planet and solar_system.selected_planet.name == body_name)
-                        if selected:
-                            # When a celestial body is selected...
-                            for body in solar_system.space_bodies:
-                                if body.name == body_name:
-                                    # Get the position and radius of the celestial body
-                                    body_position = body.compute_position(date_manager.get_current_date())
-                                    body_radius = body.radius
-                                    # Focus the camera on the celestial body
-                                    user_interactions.focus_on_body(solar_system, body_position, body_radius)
-                                    # Then set the selected celestial body to None
-                                    solar_system.selected_planet = None
-                                    break
-                    imgui.tree_pop()
+            self.populate_categories_and_handle_selection(solar_system, user_interactions, date_manager)
             imgui.end_combo()
-
+        
         # Reset item width after the combo box
         imgui.pop_item_width()
         imgui.pop_style_color(1)
         self.render_separator()
         imgui.end()
+
+    def populate_categories_and_handle_selection(self, solar_system, user_interactions, date_manager):
+        categories = self.categorize_celestial_bodies(solar_system)
+        for category, bodies in categories.items():
+            if imgui.tree_node(category):
+                for body_name in bodies:
+                    _, selected = imgui.selectable(body_name, solar_system.selected_planet and solar_system.selected_planet.name == body_name)
+                    if selected:
+                        self.handle_body_selection(solar_system, body_name, user_interactions, date_manager)
+                imgui.tree_pop()
+
+    def categorize_celestial_bodies(self, solar_system):
+        categories = {}
+        for body in solar_system.space_bodies:
+            category = body.category
+            if category not in categories:
+                categories[category] = []
+            categories[category].append(body.name)
+        return categories
+
+    def handle_body_selection(self, solar_system, body_name, user_interactions, date_manager):
+        for body in solar_system.space_bodies:
+            if body.name == body_name:
+                body_position = body.compute_position(date_manager.get_current_date())
+                body_radius = body.radius
+                user_interactions.focus_on_body(solar_system, body_position, body_radius)
+                solar_system.selected_planet = None
+                break
 
     def render_infobox(self, solar_system):
         if solar_system.is_infobox_visible() and solar_system.get_selected_planet() and solar_system.get_clicked_mouse_position():
