3835bc6f7bcc1a8191f5d852a249a57fba6a246c
MRPodle <vreg2391@outlook.com>
2023-11-15 17:56:30 -0500

    finished styling the trajectory plotting menu, need to create class file and move it now

--- core/gui_manager.py
+++ core/gui_manager.py
@@ -86,35 +86,54 @@ class GuiManager:
         return self.is_using_imgui
     
     def render_plot_trajectory_menu(self, solar_system):
-
         imgui.set_next_window_position(0, 100)
-        imgui.set_next_window_size(300, 200)
         self.set_common_style()
         imgui.begin("Plot Trajectory", flags=imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_SCROLLBAR | imgui.WINDOW_NO_MOVE | imgui.WINDOW_ALWAYS_AUTO_RESIZE)
+        imgui.push_style_color(imgui.COLOR_BUTTON, 0.0, 0.5, 0.8, 1.0)
 
         if imgui.button("Plot Trajectory"):
             self.show_trajectory_menu = not self.show_trajectory_menu
 
+        self.render_separator()
+
         if self.show_trajectory_menu:
-            # Dropdowns for selecting celestial bodies
+            dropdown_width = 150 
+
+            # Dropdowns for selecting celestial bodies, organized by categories
             for i in range(2):
                 label = f"Body {i+1}:"
                 imgui.text(label)
                 imgui.same_line()
+
+                imgui.push_item_width(dropdown_width)
+
                 current_selection = self.selected_celestial_bodies[i].name if self.selected_celestial_bodies[i] else "Select Object"
                 if imgui.begin_combo(f"##body_selector_{i}", current_selection):
-                    for body in solar_system.space_bodies:
-                        _, selected = imgui.selectable(body.name, body == self.selected_celestial_bodies[i])
-                        if selected:
-                            self.selected_celestial_bodies[i] = body
+                    categories = self.categorize_celestial_bodies(solar_system)
+                    for category, bodies in categories.items():
+                        if imgui.tree_node(category):
+                            for body_name in bodies:
+                                _, selected = imgui.selectable(body_name, solar_system.selected_planet and solar_system.selected_planet.name == body_name)
+                                if selected:
+                                    self.handle_trajectory_body_selection(i, body_name, solar_system)
+                            imgui.tree_pop()
                     imgui.end_combo()
+
+                imgui.pop_item_width()
 
             if imgui.button("Confirm"):
                 body_names = [body.name if body else "None" for body in self.selected_celestial_bodies]
                 print(f"Selected Bodies: {body_names[0]}, {body_names[1]}")
                 # Logic to plot trajectory here
-        
+
+        imgui.pop_style_color(1)
         imgui.end()
+
+    def handle_trajectory_body_selection(self, index, body_name, solar_system):
+        for body in solar_system.space_bodies:
+            if body.name == body_name:
+                self.selected_celestial_bodies[index] = body
+                break
 
     def render_loading_screen(self, progress):
         """Delegate rendering of the loading screen to the LoadingScreen instance."""
