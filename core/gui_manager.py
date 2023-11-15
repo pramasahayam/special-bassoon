2e9fe0d77e3cb4077b477104608fb14ba3e80fbe
MRPodle <vreg2391@outlook.com>
2023-11-15 18:08:36 -0500

    finished creating trajectory_menu.py and integrated TrajectoryMenu class into GuiManager class

--- core/gui_manager.py
+++ core/gui_manager.py
@@ -6,12 +6,14 @@ import imgui
 import time
 import datetime
 from imgui.integrations.pygame import PygameRenderer
-from core.loading_screen.loading_screen import LoadingScreen
+from core.gui.loading_screen import LoadingScreen
+from core.gui.trajectory_menu import TrajectoryMenu
 
 class GuiManager:
     def __init__(self, window_manager):
         self.window_manager = window_manager
         self.loading_screen = LoadingScreen(window_manager, self)
+        self.trajectory_menu = TrajectoryMenu(self.set_common_style, self.render_separator)
         self.renderer = self.setup_imgui()
         self.error_message = ""
         self.error_display_time = 0
@@ -57,8 +59,8 @@ class GuiManager:
         self.render_infobox(solar_system)
         self.render_celestial_body_selector(solar_system, user_interactions, date_manager)
         self.render_center_button(user_interactions)
+        self.trajectory_menu.render(solar_system)
         self.render_label_toggle_button()
-        self.render_plot_trajectory_menu(solar_system)
 
     def process_event(self, event):
         """
@@ -84,56 +86,6 @@ class GuiManager:
         Check if ImGui is currently capturing keyboard or mouse input.
         """
         return self.is_using_imgui
-    
-    def render_plot_trajectory_menu(self, solar_system):
-        imgui.set_next_window_position(0, 100)
-        self.set_common_style()
-        imgui.begin("Plot Trajectory", flags=imgui.WINDOW_NO_TITLE_BAR | imgui.WINDOW_NO_SCROLLBAR | imgui.WINDOW_NO_MOVE | imgui.WINDOW_ALWAYS_AUTO_RESIZE)
-        imgui.push_style_color(imgui.COLOR_BUTTON, 0.0, 0.5, 0.8, 1.0)
-
-        if imgui.button("Plot Trajectory"):
-            self.show_trajectory_menu = not self.show_trajectory_menu
-
-        self.render_separator()
-
-        if self.show_trajectory_menu:
-            dropdown_width = 150 
-
-            # Dropdowns for selecting celestial bodies, organized by categories
-            for i in range(2):
-                label = f"Body {i+1}:"
-                imgui.text(label)
-                imgui.same_line()
-
-                imgui.push_item_width(dropdown_width)
-
-                current_selection = self.selected_celestial_bodies[i].name if self.selected_celestial_bodies[i] else "Select Object"
-                if imgui.begin_combo(f"##body_selector_{i}", current_selection):
-                    categories = self.categorize_celestial_bodies(solar_system)
-                    for category, bodies in categories.items():
-                        if imgui.tree_node(category):
-                            for body_name in bodies:
-                                _, selected = imgui.selectable(body_name, solar_system.selected_planet and solar_system.selected_planet.name == body_name)
-                                if selected:
-                                    self.handle_trajectory_body_selection(i, body_name, solar_system)
-                            imgui.tree_pop()
-                    imgui.end_combo()
-
-                imgui.pop_item_width()
-
-            if imgui.button("Confirm"):
-                body_names = [body.name if body else "None" for body in self.selected_celestial_bodies]
-                print(f"Selected Bodies: {body_names[0]}, {body_names[1]}")
-                # Logic to plot trajectory here
-
-        imgui.pop_style_color(1)
-        imgui.end()
-
-    def handle_trajectory_body_selection(self, index, body_name, solar_system):
-        for body in solar_system.space_bodies:
-            if body.name == body_name:
-                self.selected_celestial_bodies[index] = body
-                break
 
     def render_loading_screen(self, progress):
         """Delegate rendering of the loading screen to the LoadingScreen instance."""
